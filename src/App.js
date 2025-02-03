ace5d85e2c687fc221d665156a33e3e847d0fb77
TaraWalk <tarawalker1310@gmail.com>
2025-02-03 11:00:47 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Tara Walker and is {""}
-        <a href="https://github.com/TaraWalk/react-project" target="_blank">
-          open-sourced on github
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by Tara Walker and is {""}
+          <a href="https://github.com/TaraWalk/react-project" target="_blank">
+            open-sourced on github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
