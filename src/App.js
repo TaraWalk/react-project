836495e5616099c57dd08dbf9c22da41193926be
TaraWalk <tarawalker1310@gmail.com>
2025-02-03 10:43:14 +0000

    Added links

--- src/App.js
+++ src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Tara Walker and is {""}
+        <a href="https://github.com/TaraWalk/react-project" target="_blank">
+          open-sourced on github
+        </a>
+      </footer>
+    </div>
+  );
 }
